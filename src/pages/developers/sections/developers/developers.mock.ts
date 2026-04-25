49c4e4f03bb691e0900c14c87bd5add64e32c2fd
AGabunia <gabuniasandro99@gmail.com>
2026-04-26 01:16:13 +0400

    add mock data for developers page

---
+++ src/pages/developers/sections/developers/developers.mock.ts
@@ -0,0 +1,57 @@
+export type DeveloperLogos = {
+  id: number;
+  name_geo: string;
+  name_end: string;
+  logo_location: string;
+};
+
+export const developerLogos: DeveloperLogos[] = [
+  {
+    id: 1,
+    name_geo: 'ბლოქსი',
+    name_end: 'Blox',
+    logo_location: 'blox.jpg'
+  },
+  {
+    id: 2,
+    name_geo: 'არქი',
+    name_end: 'Archi',
+    logo_location: 'archi.jpg'
+  },
+  {
+    id: 3,
+    name_geo: 'მ2',
+    name_end: '',
+    logo_location: 'm2.jpg'
+  },
+  {
+    id: 4,
+    name_geo: 'დეკა',
+    name_end: 'Deka',
+    logo_location: 'deka.jpg'
+  },
+  {
+    id: 5,
+    name_geo: 'ბიოგრაფი',
+    name_end: 'Biography',
+    logo_location: 'biograpi.jpg'
+  },
+  {
+    id: 6,
+    name_geo: 'აპექსი',
+    name_end: 'Apex',
+    logo_location: 'apex.jpg'
+  },
+  {
+    id: 7,
+    name_geo: 'თეთრი კვადრატი',
+    name_end: 'White Square',
+    logo_location: 'white_square.jpg'
+  },
+  {
+    id: 8,
+    name_geo: 'ისტორია',
+    name_end: 'Istoria',
+    logo_location: 'istoria.jpg'
+  }
+];
---
+++ src/pages/developers/sections/feed/developerFeed.mock.ts
@@ -0,0 +1,23 @@
+export type DevelopersFeed = {
+  id: number;
+  logo_location: string;
+};
+
+export const developersFeed: DevelopersFeed[] = [
+  {
+    id: 1,
+    logo_location: 'feed_1.jpg'
+  },
+  {
+    id: 2,
+logo_location: 'feed_1.jpg'
+  },
+  {
+    id: 3,
+    logo_location: 'feed_1.jpg'
+  },
+  {
+    id: 4,
+    logo_location: 'feed_1.jpg'
+  }
+];
